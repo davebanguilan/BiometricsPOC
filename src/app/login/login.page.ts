b421b5ef98e7a30053f4c151d68ff1554cd241c7
Dave Emilson Banguilan <dbanguilan@you-source.com>
2021-09-27 09:52:23 +0800

    MOdified code

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
-import { ModalController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -10,21 +10,51 @@ import { ModalController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
 
+  isAvailable: Promise<boolean>;
+
   constructor(
     private fingerprintAIO: FingerprintAIO,
     private router: Router,
-    private modalController: ModalController
+    private alertController: AlertController
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
   }
 
-  login() {
+  async login() {
+    this.fingerprintAIO.isAvailable()
+    .then(() => this.isAvailableSuccess())
+    .catch((error)=> this.isAvailableError(error));
+    console.log('in');
+  }
+
+  isAvailableSuccess() {
     this.fingerprintAIO.show({
+      title: 'Biometric Authentication',
+      cancelButtonTitle: 'Use Pin or Pattern',
+      disableBackup: false,
+      fallbackButtonTitle: 'FB Back Button',
+      subtitle: 'Login using biometrics',
       description: 'Biometrics login',
-    }).then(() => {
+    }).then((something) => {
       this.router.navigateByUrl('/home');
     });
+  }
+
+  async isAvailableError(error) {
+    await this.presentAlert(error);
+
+  }
+
+  async presentAlert(error) {
+    const alert = await this.alertController.create({
+      header: 'Authentication not found',
+      subHeader: 'Subtitle',
+      message: error,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
